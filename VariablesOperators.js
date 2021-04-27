df74774ce722d50f76522f8535fbc2b19fa6a371
TerriIsidro <49820744+tisidro@users.noreply.github.com>
2021-04-27 16:25:10 -0400

    2nd changes april 27

---
+++ VariablesOperators.js
@@ -0,0 +1,17 @@
+//Variables/operators
+
+const temp1 = 36.9;
+const temp2 = 37.6;
+const temp3 = 37.1;
+console.log(temp1, temp2, temp3); // Show "36.9 37.6 37.1"
+
+const a = 5.5;
+const b = 3.14;
+const c = 2 * a * b;
+console.log(c);
+
+const radius = 5.5;
+const pi = 3.14;
+const perimeter = 2 * pi * radius;
+console.log(perimeter);
+
