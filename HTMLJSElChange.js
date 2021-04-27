5e04d9b6c0b288c64096f3fc76e05240d1ed51da
TerriIsidro <49820744+tisidro@users.noreply.github.com>
2021-04-27 16:24:38 -0400

    changes from April 27th

---
+++ HTMLJSElChange.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Clicking worked! Text has changed.";
+}
+</script>
+</head>
+<body>
+
+<h2>JavaScript inserted in Head Tags</h2>
+
+<p id="demo">Some Random Paragraph.</p>
+
+<button type="button" onclick="myFunction()">Click to change text</button>
+
+</body>
+</html> 
